db6db638521c56073519ed6418276ca7603c46d7
Zac Willmington <zac.willmington@gmail.com>
2018-05-11 03:56:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,27 +8,27 @@ class PigLatinizer
     end
 
     def piglatinize(words)
-        
+
         @words = words[:user_phrase]
         binding.pry
         word_arr = []
 
-        if single_word_string?(words)
-            piglatinize_single_word(words)
+        if single_word_string?(@words)
+            piglatinize_single_word(@words)
         else
-            piglatinize_phrase_string(words)
+            piglatinize_phrase_string(@words)
         end
     end
 
-    def piglatinize_single_word(words)
+    def piglatinize_single_word(@words)
         latinized_word = []
-        word = words.split(//)
+        word = @words.split(//)
 
             vowels =["a","e","i","o","u","A","E", "O", "U"]
-            if vowels.include?(words[0])
-                latinized_word << vowel_words_piglatinize(words)
+            if vowels.include?(@words[0])
+                latinized_word << vowel_words_piglatinize(@words)
             elsif word[0] == "I" #for "I" in mid sentence
-                latinized_word << words.split(//).push("way").join
+                latinized_word << @words.split(//).push("way").join
             elsif word[0] == "i" #for "I" in mid sentence
                 latinized_word << words.split(//).push("way").join
             elsif word[0] == "p" && word[1] == "l" #consonant clusters of 2
