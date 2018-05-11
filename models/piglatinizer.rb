bf588bdb16266c50e81a5873193912b5a89ac912
Zac Willmington <zac.willmington@gmail.com>
2018-05-11 02:34:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -31,11 +31,14 @@ class PigLatinizer
                 #consonant clusters of 2
                  latinized_word <<  consonant_cluster_of_2(words)
             elsif word[0] == "s" && word[1] == "p" && word[2] == "r"#consonant clusters of 3
+                binding.pry
                 latinized_word << word.shift("ay").join
             elsif word[0] == "s" && word[1] == "p"
                 #consonant clusters of 2
+                binding.pry
                   latinized_word << word.shift("ay").join
             else #single consonant words
+                binding.pry
                 latinized_word << consonant_words(words)
             end
             latinized_word.join(" ")
