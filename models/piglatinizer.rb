e12c35825da0cb781d37ea03696ecb47fae313af
Zac Willmington <zac.willmington@gmail.com>
2018-05-11 02:58:18 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -37,7 +37,7 @@ class PigLatinizer
             elsif words.include?("spr")
                 #consonant clusters of 2
                 binding.pry
-                  latinized_word << words.split(//).shift("ay")
+                  latinized_word << words.split(//).push("ay")
             else #single consonant words
                 latinized_word << consonant_words(words)
             end
